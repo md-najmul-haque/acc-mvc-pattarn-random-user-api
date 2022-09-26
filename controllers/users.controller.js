22de1b11dd70cd145bcac0a76cbcfeae443953f5
Md Najmul Haque <najmulhaque008@gmail.com>
2022-09-26 13:34:10 +0600

    get random user controller updated

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -6,8 +6,19 @@ const controller = {};
 
 controller.getRandomUser = (req, res, next) => {
     const users = loadUser();
-    const result = users[Math.floor(Math.random() * users.length)]
-    res.status(200).send(result)
+    if (Array.isArray(users) && users.length > 0) {
+        const randomUser = users[Math.floor(Math.random() * users.length)]
+        res.status(200).json({
+            success: true,
+            message: 'random user',
+            randomUser
+        })
+    } else {
+        res.status(500).json({
+            success: false,
+            message: 'No user found',
+        })
+    }
 }
 
 

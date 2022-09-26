035fd5731eb6a760bf28fa11c59d7244bdda284d
Md Najmul Haque <najmulhaque008@gmail.com>
2022-09-26 12:05:44 +0600

    controller file created

---
+++ controllers/users.controller.js
@@ -0,0 +1,12 @@
+//controller 
+const controller = {};
+
+controller.getRandomUser = (req, res, next) => {
+
+}
+
+
+
+//export controller 
+
+module.exports = controller;
--- routes/userRoutes/users.routes.js
+++ routes/userRoutes/users.routes.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const { getRandomUser } = require('../../controllers/users.controller');
 
 //module scaffolding
 const usersRoutes = express.Router();

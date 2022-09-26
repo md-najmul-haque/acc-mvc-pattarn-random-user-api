9a517c248ce6fb68a7286e19d18184a0a174ad92
Md Najmul Haque <najmulhaque008@gmail.com>
2022-09-26 09:18:24 +0600

    comment added at index js

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
+//dependencies
 const express = require('express');
 const cors = require('cors')
-const app = express();
 const dbConnet = require('./utils/dbconnection');
 
+//app config
+const app = express();
 const port = process.env.PORT || 5000;
 
 
 // middleware
 app.use(cors());
+app.use(express.json())
 
+//database connection
 dbConnet()
 
+//base url
 app.get('/', (req, res) => {
-    res.send('Server is running')
+    res.status(200).json({
+        success: true,
+        message: "random user API server is running"
+    })
 })
 
+//create server
 app.listen(port, () => { console.log(`server running on port ${port}`) })
